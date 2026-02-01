424cdfa50268000ad7282a0ff158092ba1d11db5
Alec McCauley <alec.mccauley@outlook.com>
2026-02-01 10:02:43 -0500

    Scafolding

---
+++ coworker-app/src/renderer/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Versions from './components/Versions.svelte'
+
+</script>
+
+<div class="bg-slate-900 text-white p-8 rounded-lg shadow-xl">
+  <h1 class="text-3xl font-bold text-sky-400">
+    Electron + Svelte + Tailwind (pnpm)
+  </h1>
+</div>
+<Versions />
